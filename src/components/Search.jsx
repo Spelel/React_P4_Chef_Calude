463f7bfdedb40a396497af34ac15af0fe8c8ba48
Arek <arek-karczo.wp.pl@wp.pl>
2025-06-22 18:45:22 +0200

    form, list for ingredients and style to it

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,19 +1,48 @@
+import React from "react"
 export default function Search() {
+
+    const [ingredients, setIngredients] = React.useState([])
+
+    const ingredientsListItems = ingredients.map(ingredient => (
+        <li key={ingredient}>{ingredient}</li>
+    ))
+
+    function handleSubmit(formData) {
+        const newIngredient = formData.get("ingredient")
+        setIngredients(prevIngredients => [...prevIngredients, newIngredient])
+        // console.log(ingredients)
+    }
+
+    
     return<>
-        <section>
-            <form className="flex justify-center mt-15">
+        <section className="pl-30 pr-30" >
+            <form className="flex justify-center mt-15" action={handleSubmit}>
                 <input className="border-1 border-[#D1D5DB] p-3 w-135 rounded-lg mr-3"
                     type="text"
-                    id="ingredients"
-                    name="ingredients"
+                    id="ingredient"
+                    name="ingredient"
                     placeholder="e.g. oregano"
                     />
-                <button
-                className="bg-black text-white p-3 rounded-lg"
-                >
+                <button className="bg-black text-white p-3 rounded-lg">
                     + Add ingredient
                 </button>
             </form>
+            {
+                ingredients.length > 0 && <section>
+                <h2 className="pl-30 pt-10 text-2xl font-bold">Ingredients on hand:</h2>
+                <ul aria-live="polite" className="list-disc pl-40 pt-10">{ingredientsListItems}</ul>
+                
+                {ingredients.length > 3 &&
+                    <div className="bg-[#F0EFEB] p-8 mt-10 flex justify-between rounded-2xl">
+                    <div className="ml-10">
+                        <h3 className="text-lg">Ready for a recipe?</h3>
+                        <p className="text-[#6B7280] ">Generate a recipe from your list of ingredients.</p>
+                    </div>
+                    <button className="bg-[#D17557] p-4 mr-10 rounded-lg text-[#FAFAF8]">Get a recipe</button>
+                </div>}
+                </section>
+            }
+            
         </section>
     </>
 }
